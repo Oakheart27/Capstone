18d6cc37c774f35b47ce0546133826155f311de4
Oakheart27 <gytm337@gmail.com>
2019-01-24 20:35:59 -0700

    Skeleton of Navigation
    All back and main menu scene buttons work. Text fields missing on registration but everything else is there. There is a settings page that is empty which we can decide to use or not.

---
+++ STEM Recruitment Project/STEM Recruitment/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Functions to load new scenes 
+    public void Modules ()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("Modules");
+
+    }
+
+    public void LogIn()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("LogIn");
+
+    }
+
+    public void Register()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("Registration");
+
+    }
+
+    public void AboutUs()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("AboutUs");
+
+    }
+
+    public void Settings()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("Settings");
+
+    }
+
+    public void Back()
+    {
+        // Call to new scene
+        SceneManager.LoadScene("Home");
+
+    }
+
+    // Exits game 
+    public void quitGame()
+    {
+        // Displays message in console of Unity
+        Debug.Log("Succesful Quit");
+
+        // Quits game
+        Application.Quit(); 
+
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
