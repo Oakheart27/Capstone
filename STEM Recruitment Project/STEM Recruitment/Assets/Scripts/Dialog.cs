e654bcddbd545da14e22e42b2722f7715b84493a
Oakheart27 <gytm337@gmail.com>
2019-03-04 10:29:26 -0700

    Interview Game Stuff
    Game things

---
+++ STEM Recruitment Project/STEM Recruitment/Assets/Scripts/Dialog.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialog : MonoBehaviour
+{
+    // Intsance variables 
+    public string[] sentences;
+    private int index;
+    public int counter;  
+    public float typingSpeed;
+    public int person1, person2, person3; 
+
+    // Objects in Unity 
+    public GameObject continueBtn;
+    public GameObject choiceBtn;
+    public GameObject p1Btn;
+    public GameObject p2Btn;
+    public GameObject p3Btn;
+    public Text textDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if text displayed is current sentence index
+        if (textDisplay.text == sentences[index])
+        {
+            counter += 1; 
+            continueBtn.SetActive(true);
+
+        }
+        if (counter % 4 == 0 )
+        {
+            choiceBtn.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        continueBtn.SetActive(false); // hide continue button 
+
+        // checks if at the end of sentece
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueBtn.SetActive(false); // hide continue button 
+        }
+    }
+
+    public int getCount()
+    {
+        return counter; 
+    }
+
+    public void ButtonClick(Button btn)
+    {
+        if (btn == p1Btn)
+        {
+            person1 += 1;
+            print("Updated player 1");
+        }
+        if (btn == p2Btn) 
+        {
+            person2 += 1;
+            print("Updtated Player 2");
+        }
+        if (btn == p3Btn)
+        {
+            person3 += 1;
+            print("Updated Player 3");
+        }
+    }
+    public void bPress()
+    {
+        counter++;
+        print("count"); 
+    }
+}
---
+++ STEM Recruitment Project/STEM Recruitment/Assets/Scripts/panelChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class panelChange : MonoBehaviour
+{
+    public GameObject gamePanel;
+    public GameObject choicePanel;
+    //public GameObject backBtn;
+    int test = 1; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void changePanel()
+    {
+        gamePanel.gameObject.SetActive(false);
+        choicePanel.gameObject.SetActive(true);
+    }
+    
+    public void backPanel()
+    {
+        gamePanel.gameObject.SetActive(true);
+        choicePanel.gameObject.SetActive(false); 
+    }
+}
